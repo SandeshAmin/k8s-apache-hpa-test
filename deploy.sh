5d2a1fe05c69ec95ab72805f949c07a85c0757f8
Sandesh KV <sandeshkv92@gmail.com>
2023-07-07 23:55:16 +0530

    shell script to deploy the manifests and install metrics server

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Add Bitnami Helm repository
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+# Check if the repository was added successfully
+if [[ ! $(helm repo list | grep bitnami) ]]; then
+  echo "Error: Failed to add Bitnami Helm repository."
+  exit 1
+fi
+
+# Install metrics-server using Helm
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+helm install metrics-server bitnami/metrics-server
+
+# Check if namespace apache-test exists, create if it doesn't
+if [[ ! $(kubectl get namespace apache-test -o name) ]]; then
+  kubectl create namespace apache-test
+fi
+
+# Apply manifests from the local manifests directory
+kubectl apply -f manifests/php-apache.yaml;
+
